f787d53c443e4f875b37818dc001fe8cc792af0c
Laura Kibben <smartgirlpdx@gmail.com>
2016-12-29 10:47:13 -0800

    initial controller setup done - index working and data seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+password = Faker::Internet.password(6)
+
+5.times do
+   User.create!(
+     email: Faker::Internet.email,
+     password: password,
+     password_confirmation: password,
+     confirmed_at: Faker::Date.backward(360)
+     )
+ end
+ users = User.all
+
+ 50.times do
+   registered_application = RegisteredApplication.create!(
+     user: users.sample,
+     name: Faker::Beer.name,
+     url: Faker::Internet.url,
+     )
+    registered_application.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+ end
+ registered_applications = RegisteredApplication.all
+
+  puts "Seed finished"
+  puts "#{User.count} users created"
+  puts "#{RegisteredApplication.count} registered applications created"
