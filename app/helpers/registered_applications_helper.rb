8498ebe9ff70e99d1531c238cbc75f05a5c6b1fb
Laura Kibben <smartgirlpdx@gmail.com>
2016-12-29 13:14:23 -0800

    registered application controller working for CRUD

--- app/helpers/registered_applications_helper.rb
+++ app/helpers/registered_applications_helper.rb
@@ -1,2 +1,21 @@
 module RegisteredApplicationsHelper
+
+  def registration_owner?
+    @registered_application = RegisteredApplication.find(params[:id])
+    user = @registered_application.user
+    if user == current_user
+      true
+    end
+  end
+
+  def user_has_registrations?
+
+  end
+
+  def user_authorized_to_edit?
+  end
+
+  def user_authorized_to_edit?
+  end
+
 end
