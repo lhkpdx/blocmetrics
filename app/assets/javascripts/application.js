77628814439127f7343490268d3532c5dd2e0638
Laura Kibben <smartgirlpdx@gmail.com>
2017-01-02 13:36:26 -0800

    added ReadMe

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,8 +20,9 @@ var blocmetrics = {};
 
   blocmetrics.report = function(eventName){
     var event = {event: { name: eventName }};
+    var registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
     var request = new XMLHttpRequest();
-    request.open("POST", "http://localhost:3000/api/events", true);
+    request.open("POST", "registered_application", true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify(event));
    };
